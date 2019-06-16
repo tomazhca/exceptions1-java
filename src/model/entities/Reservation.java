7f965a53fe6232f035db825b3e4fb3201f3c1dc7
Tomaz Henrique Araujo <tomazhca@gmail.com>
2019-06-16 01:07:57 -0300

    Bad solution

--- src/model/entities/Reservation.java
+++ src/model/entities/Reservation.java
@@ -6,6 +6,8 @@ import java.util.concurrent.TimeUnit;
 
 public class Reservation {
 	
+	/*VERY BAD SOLUTION
+	
 	private Integer roomNumber;
 	private Date checkIn;
 	private Date checkOut;
@@ -55,6 +57,68 @@ public class Reservation {
 				+ ", "
 				+ duration()
 				+ " nights";
+	}*/
+	
+	//BAD SOLUTIOIN
+	
+	private Integer roomNumber;
+	private Date checkIn;
+	private Date checkOut;
+	
+	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+	
+	public Reservation(Integer roomNumber, Date checkIn, Date checkOut) {
+		this.roomNumber = roomNumber;
+		this.checkIn = checkIn;
+		this.checkOut = checkOut;
 	}
 
+	public Integer getRoomNumber() {
+		return roomNumber;
+	}
+
+	public void setRoomNumber(Integer roomNumber) {
+		this.roomNumber = roomNumber;
+	}
+
+	public Date getCheckIn() {
+		return checkIn;
+	}
+
+	public Date getCheckOut() {
+		return checkOut;
+	}
+	
+	public long duration() {
+		long diff = checkOut.getTime() - checkIn.getTime();
+		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
+	}
+	
+	public String updateDates(Date checkIn, Date checkOut) {
+		Date now = new Date();
+		if(checkIn.before(now) || checkOut.before(now)) {
+			return "Reservation dates for update must be future dates";
+		}
+		if(!checkOut.after(checkIn)) {
+			return "Check-out date must be after check-in date!";
+		}
+		this.checkIn = checkIn;
+		this.checkOut = checkOut;
+		
+		return null;
+	}
+	
+	@Override
+	public String toString() {
+		return "Room "
+				+ roomNumber
+				+ " check-In: "
+				+ sdf.format(checkIn)
+				+ " check-Out: "
+				+ sdf.format(checkOut)
+				+ ", "
+				+ duration()
+				+ " nights";
+	}
+	
 }
